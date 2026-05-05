6498b90b1164611adbaa908347e84d44ee57fb06
Keyze Torres <jsantos.torres2006@gmail.com>
2026-05-05 08:48:37 -0300

    Implement product data entry from CSV
    Added functionality to read product data from a CSV file and automate data entry using pyautogui.

--- Python PowerUp/powerup.py
+++ Python PowerUp/powerup.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import pandas as pd
 
 pyautogui.PAUSE = 1
 pyautogui.press("win")
@@ -20,3 +21,41 @@ pyautogui.press("tab")
 pyautogui.write("Orwell#1984")
 pyautogui.press("tab")
 pyautogui.press("enter")
+
+# barra de codigo do produto: Point(x=526, y=255)
+tabela = pd.read_csv("produtos.csv")
+time.sleep(5)
+
+for linha in tabela.index:
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.click(x=526, y=255)
+    pyautogui.write(codigo)
+
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.press("tab")
+    pyautogui.write(marca)
+
+    tipo = tabela.loc[linha, "marca"]
+    pyautogui.press("tab")
+    pyautogui.write(tipo)
+
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.press("tab")
+    pyautogui.write(categoria)
+
+    preco = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.press("tab")
+    pyautogui.write(preco)
+
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.press("tab")
+    pyautogui.write(custo)
+
+    # obs = tabela.loc[linha, "obs"]
+    if not pd.isna(tabela.loc[linha, "obs"]):
+        pyautogui.press("tab")
+        pyautogui.write(tabela.loc[linha, "obs"])
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
